1e18853928883ef94ae638ee26591d99d82349d1
Yuriy Syrovetskiy <cblp@cblp.su>
2017-05-30 18:55:01 +0300

    Add visualization via `gloss`

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,4 +1,21 @@
+import           Graphics.Gloss.Interface.Pure.Game
+
 main :: IO ()
-main = do
-    putStrLn "Hello World"
-    putStrLn "Hello World"
+main = play display white refreshRate initial draw onEvent onTick
+  where
+    display = InWindow "haskarium" (800, 600) (0, 0)
+    refreshRate = 60
+
+type World = Float
+
+initial :: World
+initial = 0
+
+draw :: World -> Picture
+draw time = text (show (floor time :: Int) ++ " seconds")
+
+onEvent :: Event -> World -> World
+onEvent _ world = world
+
+onTick :: Float -> World -> World
+onTick dt time = time + dt
