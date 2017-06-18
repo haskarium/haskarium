55063b966769f29ccd73a7904dde8c4d39da4f2c
Ivan Loskutov <loskutov.ivan@gmail.com>
2017-06-18 18:26:57 +0300

    Add more ants
    PR #10, fix #7

--- exe/Main.hs
+++ exe/Main.hs
@@ -6,25 +6,30 @@ main =
   where
     display = InWindow "haskarium" (800, 600) (0, 0)
     refreshRate = 60
-    startWorld = Ant{position = (0, 0), direction = 42}
+    startWorld =
+        [ Ant{position = (0, 0), direction = (-pi) / 4}
+        , Ant{position = (-30, -30), direction = 3 * pi / 4}
+        , Ant{position = (20, 40), direction = pi / 3}
+        ]
 
 type Angle = Float
 
 data Ant = Ant{position :: !Point, direction :: !Angle}
 
-type World = Ant
+type World = [Ant]
+
+drawAnt :: Ant -> Picture
+drawAnt Ant{position = (x, y)} =
+    translate x y $ color red $ rectangleSolid 10 10
 
 draw :: World -> Picture
-draw Ant{position = (x, y)} =
-    translate x y $ color red $ rectangleSolid 10 10
+draw world = pictures [drawAnt ant | ant <- world]
 
 onEvent :: Event -> World -> World
 onEvent _ world = world
 
 onTick :: Float -> World -> World
-onTick dt world = move dt ant
-  where
-    ant = world
+onTick dt world = [move dt ant | ant <- world]
 
 move :: Float -> Ant -> Ant
 move dt Ant{position = (x, y), direction = dir} = Ant
