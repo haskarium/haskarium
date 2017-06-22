b2bbd29898fe49176dcd8b9a1742a1310323fe23
Yuriy Syrovetskiy <cblp@cblp.su>
2017-06-22 15:59:13 +0300

    Add hlint config

---
+++ HLint.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE PackageImports  #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+import           "hint" HLint.Dollar
+import           "hint" HLint.HLint
+
+-- TODO(cblp, 2017-06-22) temporarily ignored
+ignore "Use ."
+ignore "Eta reduce"
