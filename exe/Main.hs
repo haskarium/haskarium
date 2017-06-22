9a1e26e0815a835ac3105845bdf0f8010aaac31e
Yuriy Syrovetskiy <cblp@cblp.su>
2017-06-22 15:59:43 +0300

    Use map where it makes sense

--- exe/Main.hs
+++ exe/Main.hs
@@ -28,13 +28,18 @@ drawAnt Ant{position = (x, y)} =
     translate x y $ color red $ rectangleSolid 10 10
 
 draw :: World -> Picture
-draw world = pictures [drawAnt ant | ant <- world]
+draw world = pictures $ map drawAnt ants
+  where
+    ants = world
 
 onEvent :: Event -> World -> World
 onEvent _ world = world
 
 onTick :: Float -> World -> World
-onTick dt world = [move dt ant | ant <- world]
+onTick dt world = map move' ants
+  where
+    ants = world
+    move' ant = move dt ant
 
 move :: Float -> Ant -> Ant
 move dt ant = ant{position = (x + dx, y + dy), direction = dir + tr * dt}
