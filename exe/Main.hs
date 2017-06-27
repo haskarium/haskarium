b445ab46efdc7035969efdc407604048b0470e3f
Yuriy Syrovetskiy <cblp@cblp.su>
2017-06-27 18:08:56 +0300

    Add sum types; Add `Fly` as an alternative to `Ant`

--- exe/Main.hs
+++ exe/Main.hs
@@ -7,53 +7,91 @@ main =
     display = InWindow "haskarium" (800, 600) (0, 0)
     refreshRate = 60
     startWorld =
-        [ Ant{position = (0, 0), direction = (-pi) / 4, turnRate = pi / 8}
-        , Ant { position = (-30, -30)
-              , direction = 3 * pi / 4
-              , turnRate = (-pi) / 3
+        [ Creature
+              { position = (0, 0)
+              , direction = - pi / 4
+              , turnRate = pi / 8
+              , species = Ant
               }
-        , Ant{position = (20, 40), direction = pi / 3, turnRate = pi / 4}
+        , Creature
+              { position = (-30, -30)
+              , direction = 3 * pi / 4
+              , turnRate = - pi / 3
+              , species = Fly
+              }
+        , Creature
+              { position = (20, 40)
+              , direction = pi / 3
+              , turnRate = pi / 4
+              , species = Ant
+              }
         ]
 
 type Angle = Float
 type RadiansPerSecond = Float
 
-data Ant = Ant
-    {position :: !Point, direction :: !Angle, turnRate :: !RadiansPerSecond}
+data Creature = Creature
+    { position  :: !Point
+    , direction :: !Angle
+    , turnRate  :: !RadiansPerSecond
+    , species   :: !Species
+    }
 
-type World = [Ant]
+data Species = Ant | Fly
+
+type World = [Creature]
 
 radiansToDegrees :: Float -> Float
 radiansToDegrees rAngle = rAngle * 180 / pi
 
-drawAnt :: Ant -> Picture
-drawAnt Ant{position = (x, y), direction = z} =
-    translate x y $ color red $ rotate (- radiansToDegrees z) antFigure
+drawCreature :: Creature -> Picture
+drawCreature Creature{position = (x, y), direction = z, species = s} =
+    translate x y $ rotate (- radiansToDegrees z) figure
   where
-    antFigure = pictures
-      [ polygon
-          [ ( 5,  0)
-          , (-5, -5)
-          , (-5,  5)
-          ]
-      , translate (-5) 0 $ circle 5
-      ]
+    figure = case s of
+        Ant -> antFigure
+        Fly -> flyFigure
+    antFigure =
+        color red $
+        pictures
+            [ polygon
+                [ ( 5,  0)
+                , (-5, -5)
+                , (-5,  5)
+                ]
+            , translate (-5) 0 $ circle 5
+            ]
+    flyFigure =
+        color green $
+        pictures
+            [ polygon
+                [ ( 5,  0)
+                , (-5, -5)
+                , (-5,  5)
+                ]
+            , translate 5  5 $ circle 5
+            , translate 5 (-5) $ circle 5
+            ]
 
 draw :: World -> Picture
-draw ants = pictures $ map drawAnt ants
+draw creatures = pictures $ map drawCreature creatures
 
 onEvent :: Event -> World -> World
 onEvent _ world = world
 
 onTick :: Float -> World -> World
-onTick dt ants = map move' ants
+onTick dt creatures = map move' creatures
   where
-    move' ant = move dt ant
+    move' creature = move dt creature
 
-move :: Float -> Ant -> Ant
-move dt ant = ant{position = (x + dx, y + dy), direction = dir + tr * dt}
+move :: Float -> Creature -> Creature
+move dt creature = creature
+    {position = (x + dx, y + dy), direction = dir + tr * dt}
   where
-    Ant{position = (x, y), direction = dir, turnRate = tr} = ant
+    Creature{position = (x, y), direction = dir, turnRate = tr, species = s} =
+        creature
     dx = speed * dt * cos dir
     dy = speed * dt * sin dir
-    speed = 20
+    speed = case s of
+        Ant -> 20
+        Fly -> 100
