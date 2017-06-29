6f28ea673b78261eab4928c9811099673aa8f579
Daria Zimarina <darya.zimarina@kaspersky.com>
2017-06-29 18:40:39 +0300

    Add some randomness to moves #14

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,40 +1,53 @@
-import           Graphics.Gloss.Interface.Pure.Game
+import          Graphics.Gloss.Interface.Pure.Game
+import          System.Random
+
+import          Debug.Trace
 
 main :: IO ()
-main =
+main = do
+    g1 <- newStdGen
+    g2 <- newStdGen
+    g3 <- newStdGen
+    let startWorld = [ Creature
+                           { position = (0, 0)
+                           , targetDir = pi / 4
+                           , currentDir = pi / 4
+                           , turnRate = pi / 4
+                           , randomGen = g1
+                           , species = Ant
+                           }
+                     , Creature
+                           { position = (-30, -30)
+                           , targetDir = 3 * pi / 4
+                           , currentDir = 3 * pi / 4
+                           , turnRate = pi / 3
+                           , randomGen = g2
+                           , species = Fly
+                           }
+                     , Creature
+                           { position = (20, 40)
+                           , targetDir = 3 * pi / 2
+                           , currentDir = 3 * pi / 2
+                           , turnRate = pi / 4
+                           , randomGen = g3
+                           , species = Ant
+                           }
+                     ]
     play display white refreshRate startWorld draw onEvent onTick
   where
     display = InWindow "haskarium" (800, 600) (0, 0)
     refreshRate = 60
-    startWorld =
-        [ Creature
-              { position = (0, 0)
-              , direction = - pi / 4
-              , turnRate = pi / 8
-              , species = Ant
-              }
-        , Creature
-              { position = (-30, -30)
-              , direction = 3 * pi / 4
-              , turnRate = - pi / 3
-              , species = Fly
-              }
-        , Creature
-              { position = (20, 40)
-              , direction = pi / 3
-              , turnRate = pi / 4
-              , species = Ant
-              }
-        ]
 
 type Angle = Float
 type RadiansPerSecond = Float
 
 data Creature = Creature
-    { position  :: !Point
-    , direction :: !Angle
-    , turnRate  :: !RadiansPerSecond
-    , species   :: !Species
+    { position   :: !Point
+    , targetDir  :: !Angle
+    , currentDir :: !Angle
+    , turnRate   :: !RadiansPerSecond
+    , randomGen  :: !StdGen
+    , species    :: !Species
     }
 
 data Species = Ant | Fly
@@ -45,7 +58,7 @@ radiansToDegrees :: Float -> Float
 radiansToDegrees rAngle = rAngle * 180 / pi
 
 drawCreature :: Creature -> Picture
-drawCreature Creature{position = (x, y), direction = z, species = s} =
+drawCreature Creature{position = (x, y), currentDir = z, species = s} =
     translate x y $ rotate (- radiansToDegrees z) figure
   where
     figure = case s of
@@ -86,12 +99,32 @@ onTick dt creatures = map move' creatures
 
 move :: Float -> Creature -> Creature
 move dt creature = creature
-    {position = (x + dx, y + dy), direction = dir + tr * dt}
+    { position = (x + dx, y + dy)
+    , targetDir = newTdir
+    , currentDir = newCdir
+    , randomGen = g'
+    }
   where
-    Creature{position = (x, y), direction = dir, turnRate = tr, species = s} =
-        creature
-    dx = speed * dt * cos dir
-    dy = speed * dt * sin dir
+    Creature { position = (x, y)
+             , targetDir = tdir
+             , currentDir = cdir
+             , turnRate = tr
+             , randomGen = g
+             , species = s
+             } = creature
+    (newTdir, g') = trace dbg $ if (cdir <= tdir && cdir' >= tdir) || (cdir >= tdir && cdir' <= tdir)
+                                then randomR (0, 2 * pi) g
+                                else (tdir, g)
+    newCdir
+      | cdir' < 0       = cdir' + 2 * pi
+      | cdir' > 2 * pi  = cdir' - 2 * pi
+      | otherwise       = cdir'
+    cdir' = cdir + trSign * tr * dt
+    trSign = if (delta > -pi && delta < 0) || delta > pi then -1 else 1
+        where delta = tdir - cdir
+    dx = speed * dt * cos cdir
+    dy = speed * dt * sin cdir
     speed = case s of
-        Ant -> 20
+        Ant -> 30
         Fly -> 100
+    dbg = unwords $ map show [tdir, cdir, abs (tdir - cdir'), trSign]
