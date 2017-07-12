43f9468285f19b787d02b3ba256703cb9ec9edd5
Daria Zimarina <darya.zimarina@kaspersky.com>
2017-07-12 18:25:35 +0300

    Add some randomness to moves #14

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,56 +1,65 @@
 {-# LANGUAGE LambdaCase     #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
-import           Graphics.Gloss.Interface.Pure.Game
+import          Graphics.Gloss.Interface.Pure.Game
+import          System.Random
 
-main :: IO ()
-main =
+main = do
+    g <- newStdGen
+    let startWorld = World{randomGen = g, creatures = cs}
     play display white refreshRate startWorld draw onEvent onTick
   where
+    cs =
+        [ Creature
+            { position = (0, 0)
+             , targetDir = pi / 4
+             , currentDir = pi / 4
+             , turnRate = pi / 4
+             , species = Ant
+             }
+        , Creature
+             { position = (-30, -30)
+             , targetDir = 3 * pi / 4
+             , currentDir = 3 * pi / 4
+             , turnRate = pi / 2
+             , species = Fly
+             }
+        , Creature
+             { position = (20, 40)
+             , targetDir = 3 * pi / 2
+             , currentDir = 3 * pi / 2
+             , turnRate = pi / 4
+             , species = Flea{idleTime = 0}
+             }
+        ]
     display = InWindow "haskarium" (800, 600) (0, 0)
     refreshRate = 60
-    startWorld =
-        [ Creature
-              { position = (0, 0)
-              , direction = - pi / 4
-              , turnRate = pi / 8
-              , species = Ant
-              }
-        , Creature
-              { position = (-30, -30)
-              , direction = 3 * pi / 4
-              , turnRate = - pi / 3
-              , species = Fly
-              }
-        , Creature
-              { position = (20, 40)
-              , direction = pi / 3
-              , turnRate = pi / 4
-              , species = Flea{idleTime = 0}
-              }
-        ]
 
 type Angle = Float
 type RadiansPerSecond = Float
 
 data Creature = Creature
-    { position  :: !Point
-    , direction :: !Angle
-    , turnRate  :: !RadiansPerSecond
-    , species   :: !Species
+    { position   :: !Point
+    , targetDir  :: !Angle
+    , currentDir :: !Angle
+    , turnRate   :: !RadiansPerSecond
+    , species    :: !Species
     }
 
 data Species = Ant | Flea{idleTime :: !Float} | Fly
 
-type World = [Creature]
+data World = World
+    { randomGen :: !StdGen
+    , creatures :: ![Creature]
+    }
 
 radiansToDegrees :: Float -> Float
 radiansToDegrees rAngle = rAngle * 180 / pi
 
 drawCreature :: Creature -> Picture
-drawCreature Creature{position = (x, y), direction, species} =
+drawCreature Creature{position = (x, y), currentDir, species} =
     translate x y $
-    rotate (- radiansToDegrees direction) $
+    rotate (- radiansToDegrees currentDir) $
     figure species
 
 figure :: Species -> Picture
@@ -82,41 +91,61 @@ figure = \case
         ]
 
 draw :: World -> Picture
-draw creatures = pictures $ map drawCreature creatures
+draw World{creatures} = pictures $ map drawCreature creatures
 
 onEvent :: Event -> World -> World
 onEvent _ world = world
 
 onTick :: Float -> World -> World
-onTick dt creatures = map update' creatures
-  where
-    update' creature = updateCreature dt creature
+onTick dt w@World{creatures} = foldr (updateCreature dt) w{creatures = []} creatures
 
-updateCreature :: Float -> Creature -> Creature
-updateCreature dt creature = case species of
-    Ant            -> run 20
-    Fly            -> run 200
-    Flea{idleTime} -> jump idleTime 100
+updateCreature :: Float -> Creature -> World -> World
+updateCreature dt c@Creature{species} World{randomGen = g, creatures = cs} =
+    World{randomGen = g', creatures = (c' : cs)}
   where
-    Creature{position = (x, y), direction, turnRate, species} =
-        creature
-    run speed = creatureMovedTurned dx dy
-      where
-        dx = speed * dt * cos direction
-        dy = speed * dt * sin direction
-    creatureMovedTurned dx dy = creature
-        { position = (x + dx, y + dy)
-        , direction = direction + turnRate * dt
-        }
-    jump idleTime distance =
-        if idleTime < fleaMaxIdleTime then
-            (creatureMovedTurned 0 0){species = Flea{idleTime = idleTime + dt}}
-        else let
-            dx = distance * cos direction
-            dy = distance * sin direction
-            in
-            (creatureMovedTurned dx dy)
-                {species = Flea{idleTime = idleTime + dt - fleaMaxIdleTime}}
+    (g', c') = turn g dt $ move dt c
+    move = case species of
+        Ant            -> run 30
+        Fly            -> run 200
+        Flea{idleTime} -> jump idleTime 100
+
+turn :: StdGen -> Float -> Creature -> (StdGen, Creature)
+turn g dt creature = (g', creature{targetDir = newTdir, currentDir = newCdir})
+  where
+    Creature { targetDir = tdir
+             , currentDir = cdir
+             , turnRate = tr
+             } = creature
+    (newTdir, g') = if (cdir <= tdir && cdir' >= tdir) || (cdir >= tdir && cdir' <= tdir)
+                    then randomR (0, 2 * pi) g
+                    else (tdir, g)
+    newCdir
+      | cdir' < 0       = cdir' + 2 * pi
+      | cdir' > 2 * pi  = cdir' - 2 * pi
+      | otherwise       = cdir'
+    cdir' = cdir + trSign * tr * dt
+    trSign = if (delta > -pi && delta < 0) || delta > pi then -1 else 1
+    delta = tdir - cdir
+
+run :: Float -> Float -> Creature -> Creature
+run speed dt c@Creature{position = (x, y), currentDir = dir} = 
+    c{position = (x + dx, y + dy)}
+  where
+    dx = speed * dt * cos dir
+    dy = speed * dt * sin dir
+
+jump :: Float -> Float -> Float -> Creature -> Creature
+jump idleTime distance dt c@Creature{position = (x, y), currentDir = dir} =
+    if idleTime < fleaMaxIdleTime then
+        c{species = Flea{idleTime = idleTime + dt}}
+    else
+      let
+        dx = distance * cos dir
+        dy = distance * sin dir
+      in
+        c { position = (x + dx, y + dy)
+          , species = Flea{idleTime = idleTime + dt - fleaMaxIdleTime}}
 
 fleaMaxIdleTime :: Float
 fleaMaxIdleTime = 2
+
